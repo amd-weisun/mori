6d724f2696ce71c63c5274a1864825c890f533b2
amd-weisun <Wei.Sun@amd.com>
2025-12-11 13:40:28 +0000

    [wip] fix include

--- include/mori/ops/dispatch_combine/layout_transform_kernels.hpp
+++ include/mori/ops/dispatch_combine/layout_transform_kernels.hpp
@@ -2,6 +2,8 @@
 
 #include <hip/hip_runtime.h>
 #include "mori/core/core.hpp"
+#include <hip/hip_fp8.h>
+#include <hip/library_types.h>
 
 namespace mori {
 namespace moe {
@@ -9,7 +11,7 @@ namespace moe {
 template <typename T>
 void LaunchTransformDispatchOutput(
     const T* src, T* dst,
-    const index_t* indices, const index_t* expert_ids, const index_t* slot_ids,
+    const index_t * indices, const index_t* expert_ids, const index_t* slot_ids,
     int64_t stride_src_n, int64_t stride_src_h,
     int64_t stride_dst_e, int64_t stride_dst_c, int64_t stride_dst_h,
     int num_tokens, int H, hipStream_t stream);
