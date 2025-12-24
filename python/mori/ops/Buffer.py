a16040ece75121732f7cdd6a354865618cf07190
amd-weisun <Wei.Sun@amd.com>
2025-12-24 13:38:30 +0000

    [wip] remove prints

--- python/mori/ops/Buffer.py
+++ python/mori/ops/Buffer.py
@@ -486,14 +486,14 @@ class Buffer:
 
         topk_idx = topk_idx.to(dtype=torch.int32)
 
-        if self.rank == 0:
-            print(f"[Rank {self.rank}] Low latency combining with dtype={dtype}, hidden_dim={hidden_dim}, num_tokens={rec_output.size(0)}")
-            print(f"rec_output shape = {rec_output.shape}")    
-            print(f"rec_output = {rec_output}") 
-            print(f"[topk_weights shape] = {topk_weights.shape}")
-            print(f"[topk_weights] = {topk_weights}")
-            print(f"[dispatch_weights shape] = {dispatch_weights.shape}")
-            print(f"[dispatch_weights] = {dispatch_weights}")
+        # if self.rank == 0:
+        #     print(f"[Rank {self.rank}] Low latency combining with dtype={dtype}, hidden_dim={hidden_dim}, num_tokens={rec_output.size(0)}")
+        #     print(f"rec_output shape = {rec_output.shape}")    
+        #     print(f"rec_output = {rec_output}") 
+        #     print(f"[topk_weights shape] = {topk_weights.shape}")
+        #     print(f"[topk_weights] = {topk_weights}")
+        #     print(f"[dispatch_weights shape] = {dispatch_weights.shape}")
+        #     print(f"[dispatch_weights] = {dispatch_weights}")
 
         combine_output,combine_output_weight = op.combine(
             rec_output,
@@ -504,14 +504,14 @@ class Buffer:
             warp_per_block=16,
         )
 
-        if(self.rank == 0):
-            print(f"[Rank {self.rank}] Combining with dtype={dtype}, hidden_dim={hidden_dim}, num_tokens={rec_output.size(0)}")
-            print(f"[inp shape {rec_output.shape}]" )
-            print(f"rec_output = {rec_output}") 
-            print(f"[topk_weights] = {topk_weights}")
-            print(f"[dispatch_weights] = {dispatch_weights}")
-            print(f"[combine_output_weight] = {combine_output_weight}")
-            print(f"[combine_output] = {combine_output}")
+        # if(self.rank == 0):
+        #     print(f"[Rank {self.rank}] Combining with dtype={dtype}, hidden_dim={hidden_dim}, num_tokens={rec_output.size(0)}")
+        #     print(f"[inp shape {rec_output.shape}]" )
+        #     print(f"rec_output = {rec_output}") 
+        #     print(f"[topk_weights] = {topk_weights}")
+        #     print(f"[dispatch_weights] = {dispatch_weights}")
+        #     print(f"[combine_output_weight] = {combine_output_weight}")
+        #     print(f"[combine_output] = {combine_output}")
 
         return combine_output, EventOverlap(), None
         
